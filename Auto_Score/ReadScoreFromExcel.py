9d1fe3e6d05e433c58e154b9afea982de8cef457
qseft0402 <qseft0402@gamil.com>
2023-04-24 19:42:18 +0800

    20230423

--- Auto_Score/ReadScoreFromExcel.py
+++ Auto_Score/ReadScoreFromExcel.py
@@ -3,14 +3,17 @@ import pandas as pd
 class RSFE:
 
 
-    def __init__(self,exlPath):
+    def __init__(self,exlPath,excelColIDName,excelColScoreName):
         self.df = pd.DataFrame()
         self.df = pd.read_excel(exlPath)
+        self.excelColScoreName=excelColScoreName
+        self.excelColIDName=excelColIDName
+
     def getScore(self,stdID):
         for index, row in self.df.iterrows():
             name = row['姓名']
-            std_id = row['帳號']
-            score = row['最終成績']
+            std_id = row[self.excelColIDName] #帳號、學號
+            score = row[self.excelColScoreName] #最終成績
             print(std_id, stdID)
             if std_id==stdID and name!=None:
                 print('return!!: '+str(score))
